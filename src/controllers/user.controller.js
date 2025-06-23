26296b4e0e1f9a053bb68628bd96abf317ef08ba
kanhaiya kumar <kk7077574@gmail.com>
2025-06-23 17:07:17 +0530

    add functionality to update user avatar and cover image

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -282,6 +282,68 @@ const logoutUser = asyncHandler(async(req,res) => {
         .json(new ApiResponse(200, User ,"Account details updated successfully"))
     })
 
+// Updating avatar
+
+    const updateUserAvatar = asyncHandler(async(req, res) =>{
+        const avatarLocalPath = req.file?.path
+
+        if(!avatarLocalPath){
+            throw new ApiError(400,"Avatar file is missing")
+        }
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+        if(!avatar.url){
+            throw new ApiError(400, "Error while uploading avatar on cloudinary")
+        }
+        const user = await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set:{
+                    avatar: avatar.url
+                }
+            },
+            {
+                new: true
+            }
+        ).select("-password")
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,user, "avatar updated successfully")
+        )
+    })
+// updating coverImage
+      const updateUserCoverImage = asyncHandler(async(req, res) =>{
+        const coverImageLocalPath = req.file?.path
+
+        if(!coverImageLocalPath){
+            throw new ApiError(400,"Cover Image file is missing")
+        }
+        const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+        if(!coverImage.url){
+            throw new ApiError(400, "Error while updated avatar on cloudinary")
+        }
+        const user = await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set:{
+                    coverImage: coverImage.url
+                }
+            },
+            {
+                new: true
+            }
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,user, "Cover Image uploaded successfully")
+        )
+    })
+
+
 
 export {
      registerUser,
@@ -290,5 +352,6 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails
+    updateAccountDetails,
+    updateUserAvatar
     }
